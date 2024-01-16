4319d58b9b14559872aa2202eb4b865f8a278766
Sagar Dhadke <peaceunique2003@gmail.com>
2024-01-16 20:48:31 +0530

    Go Ride Layout

---
+++ app/src/main/java/com/sagardhadke/androidlayout/Constraintlayout/Login_GoRide.kt
@@ -0,0 +1,14 @@
+package com.sagardhadke.androidlayout.Constraintlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sagardhadke.androidlayout.R
+
+class Login_GoRide : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_go_ride)
+    }
+
+
+}

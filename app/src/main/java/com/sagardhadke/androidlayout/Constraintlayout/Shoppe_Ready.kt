72908b9765ade5665699cf3fbfb310a39d51f43b
Sagar Dhadke <peaceunique2003@gmail.com>
2024-01-18 13:40:00 +0530

    Shoppe Ready UI

---
+++ app/src/main/java/com/sagardhadke/androidlayout/Constraintlayout/Shoppe_Ready.kt
@@ -0,0 +1,12 @@
+package com.sagardhadke.androidlayout.Constraintlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sagardhadke.androidlayout.R
+
+class Shoppe_Ready : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shoppe_ready)
+    }
+}

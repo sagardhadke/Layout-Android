bfd5cfada81d716acd2951202900fbda30b59ae1
Sagar Dhadke <peaceunique2003@gmail.com>
2024-01-18 16:41:05 +0530

    Grocery Us Register UI

---
+++ app/src/main/java/com/sagardhadke/androidlayout/LinearLayout/GroceryUs_signup.kt
@@ -0,0 +1,12 @@
+package com.sagardhadke.androidlayout.LinearLayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sagardhadke.androidlayout.R
+
+class GroceryUs_signup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_grocery_us_signup)
+    }
+}

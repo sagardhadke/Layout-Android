ec33030040fcb4c51f4dd1d01e2e706539f58183
Sagar Dhadke <peaceunique2003@gmail.com>
2024-01-19 21:19:02 +0530

    Shoppe Order Receive

---
+++ app/src/main/java/com/sagardhadke/androidlayout/LinearLayout/Shoppe_Order_Receieve.kt
@@ -0,0 +1,12 @@
+package com.sagardhadke.androidlayout.LinearLayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sagardhadke.androidlayout.R
+
+class Shoppe_Order_Receieve : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shoppe_order_recieve)
+    }
+}

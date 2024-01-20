167cb41cc6abd895ddbd9d964f0a5194fae1ae3a
Sagar Dhadke <peaceunique2003@gmail.com>
2024-01-20 18:54:47 +0530

    Premium UI

---
+++ app/src/main/java/com/sagardhadke/androidlayout/RelativeLayout/Premium.kt
@@ -0,0 +1,12 @@
+package com.sagardhadke.androidlayout.RelativeLayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sagardhadke.androidlayout.R
+
+class Premium : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_premium)
+    }
+}

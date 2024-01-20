92e6dec526a262b77e354e0c265ec53d96e3b3b1
Sagar Dhadke <peaceunique2003@gmail.com>
2024-01-20 20:18:12 +0530

    Instagram Login UI

---
+++ app/src/main/java/com/sagardhadke/androidlayout/RelativeLayout/Instagram_login.kt
@@ -0,0 +1,12 @@
+package com.sagardhadke.androidlayout.RelativeLayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sagardhadke.androidlayout.R
+
+class Instagram_login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_instagram_login)
+    }
+}

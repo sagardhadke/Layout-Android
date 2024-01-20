576498b9011211897b6549e27e90b1e9d0e419e2
Sagar Dhadke <peaceunique2003@gmail.com>
2024-01-20 17:31:11 +0530

    Join Premium

---
+++ app/src/main/java/com/sagardhadke/androidlayout/RelativeLayout/Join_Premium.kt
@@ -0,0 +1,12 @@
+package com.sagardhadke.androidlayout.RelativeLayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sagardhadke.androidlayout.R
+
+class Join_Premium : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join_premium)
+    }
+}

1c39064f8417e0721be96e1233ff149c6cbf2ffd
Sagar Dhadke <peaceunique2003@gmail.com>
2024-01-21 20:58:54 +0530

    GridLayout

---
+++ app/src/main/java/com/sagardhadke/androidlayout/GridLayout/Grid.kt
@@ -0,0 +1,12 @@
+package com.sagardhadke.androidlayout.GridLayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sagardhadke.androidlayout.R
+
+class Grid : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_grid)
+    }
+}
